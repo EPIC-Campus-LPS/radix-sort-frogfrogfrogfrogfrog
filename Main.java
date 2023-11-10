d11ed7337bce709c08b588e144e7820881b3b39b
cardinn454_lpsk12 <Cardinn454@lpsk12.org>
2023-11-10 11:28:02 -0700

    Started radix sort

--- Main.java
+++ Main.java
@@ -1,6 +1,15 @@
+import java.io.FileNotFoundException;
+
 public class Main {
-    public static void main(String[] args){
-        RadixSort hi = new RadixSort();
-        hi.radixSort();
+    public static void main(String[] args) throws FileNotFoundException {
+        RadixSort radixSort = new RadixSort();
+
+        String[] result = radixSort.radixSort();
+
+        if (result != null){
+            for (String element : result){
+                System.out.println(element);
+            }
+        }
     }
 }
--- RadixSort.java
+++ RadixSort.java
@@ -1,11 +1,80 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.HashMap;
+import java.util.LinkedList;
 
 public class RadixSort {
-    public String[] radixSort() {
+    public String[] radixSort() throws FileNotFoundException {
 
+        LinkedList<String> fileContents = new LinkedList<>();
+        File file = new File("text.txt");
+        Scanner scan = new Scanner(file);
 
+        //scans through the entire file and puts it inside a linked list while also finding the longest length string
+        int longestLength = 0;
 
+        while (scan.hasNextLine()){
+            String data = scan.nextLine();
+            fileContents.add(data);
+
+            if(data.length() > longestLength){
+                longestLength = data.length();
+            }
+        }
+
+        //goes through the entire linked list
+        for(int i = 0; i < fileContents.size(); i++){
+
+            //pads the indexed element with spaces until length of longest string
+            if(fileContents.get(i).length() < longestLength){
+
+                String paddedWord = fileContents.get(i);
+
+                while (paddedWord.length() < longestLength){
+                    paddedWord = paddedWord + " ";
+                }
+
+                fileContents.set(i,paddedWord);
+            }
+        }
+
+        //initializes a hashmap and a linked list to put inside the hashmap
+        HashMap<String, LinkedList<String>> alphabetTable = new HashMap<>();
+
+        return null;
     }
+
+    /*
+    public String[] radixSort() throws FileNotFoundException {
+
+        //initializes a hashmap and a linked list to put inside the hashmap
+        HashMap<String, LinkedList<String>> alphabetTable = new HashMap<>();
+        LinkedList<String> list = new LinkedList<>();
+
+        for (char index = 'a'; index <= 'z'; ++index) {
+            alphabetTable.put(Character.toString(index), new LinkedList<>());
+        }
+
+        //reads the text file and puts it in the hashmap alphabetically
+        File file = new File("text.txt");
+        Scanner scan = new Scanner(file);
+
+        while (scan.hasNextLine()){
+            String data = scan.nextLine();
+            String firstChar = String.valueOf(data.charAt(0)).toLowerCase();
+            LinkedList<String> listModified = alphabetTable.get(firstChar);
+            listModified.add(data);
+        }
+
+        // Print the entire HashMap
+        for (String key : alphabetTable.keySet()) {
+            LinkedList<String> value = alphabetTable.get(key);
+            System.out.println("Key: " + key + ", List: " + value);
+        }
+
+        return null;
+    }
+
+     */
 }
